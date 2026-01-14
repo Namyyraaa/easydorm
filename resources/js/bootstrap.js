f8975fc991dedf8d64881bc1a4cbd67375e743fa
Namyyraaa PC <nur_ameera_bi22@iluv.ums.edu.my>
2026-01-15 03:17:11 +0800

    deployment fix JS wrapper https

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,19 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+if (typeof window !== 'undefined' && typeof window.route === 'function') {
+    const originalRoute = window.route;
+
+    window.route = (name, params = {}, absolute = false, config) => {
+        const fullUrl = originalRoute(name, params, absolute, config);
+
+        try {
+            const u = new URL(fullUrl, window.location.origin);
+            // always return relative URL: /path?query#hash
+            return u.pathname + u.search + u.hash;
+        } catch {
+            return fullUrl;
+        }
+    };
+}
