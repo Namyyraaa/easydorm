0c20fd47734254b2f81133a07477343a7de89a38
Namyyraaa PC <nur_ameera_bi22@iluv.ums.edu.my>
2026-01-15 03:59:19 +0800

    deployment fix force https v2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,14 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Inertia\Ssr\Gateway; // whatever you already have
+use Inertia\Ssr\HttpGateway;
+use Inertia\Ssr\NullGateway;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Vite;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,10 +27,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
 
-        // Force HTTPS in production 
-        if (config('app.env') === 'production') {
+        // Force all generated URLs (redirects, route(), asset()) to use HTTPS in production
+        if (App::environment('production')) {
             URL::forceScheme('https');
         }
+
         Vite::prefetch(concurrency: 3);
     }
 }
